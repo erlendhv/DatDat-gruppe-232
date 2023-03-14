9ce725020b12d671388353d1a74127a881638978
Erlend Veire <erlendhv@stud.ntnu.no>
2023-03-14 11:56:34 +0100

    feat: added userstory c

--- DB2.py
+++ DB2.py
@@ -3,22 +3,27 @@ import sqlite3
 con = sqlite3.connect('232DB.db')
 cursor = con.cursor()
 
-# stasjon = input("Skriv inn stasjon: ")
+
+# Brukerhistorie c
+stasjon = input("Skriv inn stasjon: ")
 dag = input("Skriv inn dag: ")
 
-cursor.execute("SELECT * FROM TogruteForekomst WHERE Ukedag = ?", (dag,))
+cursor.execute("select TogruteID from StasjonerITabell natural join (select * from Togrutetabell natural join (select * from TogruteForekomst where Ukedag = ?)) where Stasjonsnavn = ?", (dag, stasjon))
 forekomster = cursor.fetchall()
 print(forekomster)
 
 
-
-
-Kundenummer = int(input("Skriv inn Kundenummer: "))
+# Brukerhistorie e
+Kundenummer = 0
+cursor.execute("select Kundenummer from Kunde")
+Kundenummer = cursor.fetchall()[-1][0]
+Kundenummer += 1
 Kundenavn = input("Skriv inn navnet ditt: ")
 Epost = input("Skriv inn E-post: ")
 Mobilnummer = input("Skriv inn mobilnummer: ")
 
-cursor.execute('''INSERT INTO Kunde VALUES (?, ?, ?, ?)''', (Kundenummer, Kundenavn, Epost, Mobilnummer))
+cursor.execute('''INSERT INTO Kunde VALUES (?, ?, ?, ?)''',
+               (Kundenummer, Kundenavn, Epost, Mobilnummer))
 
 
 con.commit()
