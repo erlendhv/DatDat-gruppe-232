a8d3ef77a6a04f616a98ab686c807e396829bcc8
edvardgg <edvardgg@ntnu.no>
2023-03-14 10:28:14 +0100

    e: en bruker kan nå registrere seg

--- DB2.py
+++ DB2.py
@@ -10,4 +10,17 @@ cursor.execute("SELECT * FROM TogruteForekomst WHERE Ukedag = ?", (dag,))
 forekomster = cursor.fetchall()
 print(forekomster)
 
+
+
+
+Kundenummer = int(input("Skriv inn Kundenummer: "))
+Kundenavn = input("Skriv inn navnet ditt: ")
+Epost = input("Skriv inn E-post: ")
+Mobilnummer = input("Skriv inn mobilnummer: ")
+
+cursor.execute('''INSERT INTO Kunde VALUES (?, ?, ?, ?)''', (Kundenummer, Kundenavn, Epost, Mobilnummer))
+
+
+con.commit()
+
 con.close()
