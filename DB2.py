9ba151f26a17102d8f141f5ae9f8cf4e5fc2ed6c
Erlend Veire <erlendhv@stud.ntnu.no>
2023-03-14 10:04:29 +0100

    fix: added correct database file

--- DB2.py
+++ DB2.py
@@ -1,1 +1,13 @@
 # New file
+import sqlite3
+con = sqlite3.connect('232DB.db')
+cursor = con.cursor()
+
+# stasjon = input("Skriv inn stasjon: ")
+dag = input("Skriv inn dag: ")
+
+cursor.execute("SELECT * FROM TogruteForekomst WHERE Ukedag = ?", (dag,))
+forekomster = cursor.fetchall()
+print(forekomster)
+
+con.close()
