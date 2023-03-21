cf2db61d7c6f6894bbb1140703ab227ffbf19ed9
Erlend Veire <erlendhv@stud.ntnu.no>
2023-03-21 18:32:49 +0100

    comment: added comment

--- DB2.py
+++ DB2.py
@@ -4,6 +4,7 @@ from python.brukerhistorie_E import brukerhistorie_e
 from python.brukerhistorie_G import brukerhistorie_g
 from python.brukerhistorie_H import brukerhistorie_h
 
+# For å teste en brukerhistorie, fjern kommentar på den du ønsker å teste
 if __name__ == "__main__":
     print("Velkommen til Togtider AS")
     # brukerhistorie_c()
